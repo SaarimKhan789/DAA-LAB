dc023b658101ab289789060b1859cc4cd075c6ec
Saarim Khan <saarimkhan515@gmail.com>
2021-07-27 14:39:13 +0530

    Intial Commit

---
+++ Week 1/LinearSearch/LinearSearch.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+    ifstream in;
+    in.open("Input.txt");
+    ofstream out;
+    out.open("Output.txt");
+
+    int t;
+    in >> t;
+
+    while (t--)
+    {
+
+        int n;
+        in >> n;
+
+        int arr[1000];
+
+        for (int i = 0; i < n; i++)
+        {
+            in >> arr[i];
+        }
+
+        int key;
+        in >> key;
+        int comparisons = 0;
+        bool flag = false;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            ++comparisons;
+            if (arr[i] == key)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            out << "Present"
+                << " " << comparisons << endl;
+        }
+        else
+        {
+            out << "Not Present"
+                << " " << comparisons << endl;
+        }
+    }
+}
