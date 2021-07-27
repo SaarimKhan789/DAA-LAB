32b447a4222cfa25b594966b50447c7271574c4e
Saarim Khan <saarimkhan515@gmail.com>
2021-07-27 13:03:37 +0530

    Intial commit

---
+++ Week 0/linear_search.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int n;
+        cin>>n;
+
+        int arr[1000];
+
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+
+        int key;
+        cin>>key;
+        int comparisons=0;
+        bool flag=false;
+
+        for(int i=0;i<n;i++){
+            
+            ++comparisons;
+            if(arr[i]==key){
+                flag=true;   
+                break;
+            }
+
+             
+        }
+
+        if(flag){
+            cout<<"Element is Present"<<endl;
+        }
+        else{
+            cout<<"Element is not Present"<<endl;
+        }
+
+        cout<<"Total no of comparisons="<<comparisons<<endl;
+    }
+}
