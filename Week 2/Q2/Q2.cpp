9a8cc367be335a4774f180c77a17bcc1ef74b9c4
Saarim Khan <saarimkhan515@gmail.com>
2021-08-05 22:02:34 +0530

    Intial Commit

---
+++ Week 2/Q2/Q2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    ifstream in;
+    in.open("Input.txt");
+    ofstream out;
+    out.open("Output.txt");
+    int t;
+    in >> t;
+    while (t--)
+    {
+        int n;
+        in >> n;
+        int *arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            in >> arr[i];
+        }
+        bool flag = false;
+
+        for (int k = 0; k < n; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+
+                    if (i != k && j != k)
+                    {
+
+                        int sum = arr[i] + arr[j];
+
+                        if (arr[k] == sum)
+                        {
+                            flag = true;
+                            out << i + 1 << " " << j + 1 << " " << k + 1 << endl;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!flag)
+        {
+            out << "No sequence found" << endl;
+        }
+    }
+    return 0;
+}
